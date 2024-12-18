35272a0b8b522bacfdbe7174867f429ea959bd83
Sanjay Singh <sanjay007abd@gmail.com>
2024-12-18 18:29:28 +0530

    prettier homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import CategoryPage from './components/pages/categoryPage/categoryPage.component'
-import data from './components/pages/categoryPage/utils.data'
-import NavBar from '../src/components/NavBar/NavBar.components'
-import ProductPage from './components/pages/ProductPage/ProductPage.component'
-import CartPage from './components/pages/cartPage/cartPage.component'
-import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import HomePage from './components/pages/HomePage/HomePage.component'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CategoryPage from "./components/pages/categoryPage/categoryPage.component";
+import data from "./components/pages/categoryPage/utils.data";
+import NavBar from "../src/components/NavBar/NavBar.components";
+import ProductPage from "./components/pages/ProductPage/ProductPage.component";
+import CartPage from "./components/pages/cartPage/cartPage.component";
+import SignInAndSignUpPage from "./components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import HomePage from "./components/pages/HomePage/HomePage.component";
 
 function App() {
-  
-
   return (
     <>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path="/shop/:category" element={<CategoryPage  categories = {data}/>}/>
-      <Route path="/shop/product/:id" element={<ProductPage products = {data} />}></Route>
-      <Route path="/shop/cart" element={<CartPage/>}></Route>
-      <Route path="/signin" element={<SignInAndSignUpPage/>}></Route>
-    </Routes>
-      
-
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/shop/:category"
+          element={<CategoryPage categories={data} />}
+        />
+        <Route
+          path="/shop/product/:id"
+          element={<ProductPage products={data} />}
+        ></Route>
+        <Route path="/shop/cart" element={<CartPage />}></Route>
+        <Route path="/signin" element={<SignInAndSignUpPage />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/pages/homepage/homePage.component.jsx
+++ src/components/pages/homepage/homePage.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './HomePage.style.scss'
+import "./HomePage.style.scss";
 import Carousel from "../../Carousel/Carousel.component";
 import BrandCategory from "../../brandCategory/brandCategory.component";
 import Category from "../../Category/Category.component";
@@ -8,41 +8,41 @@ import { Link } from "react-router-dom";
 import data from "../categoryPage/utils.data";
 
 const HomePage = () => {
+  const top_pick_data = data.flatMap((top_pick) => top_pick.items).slice(7, 10);
 
-    const top_pick_data = data.flatMap((top_pick) => top_pick.items).slice(7,10)
-
-    return(
-        <>
-      
-        <Carousel/>
-         <BrandCategory/> 
-         <div className="top-picks-container">
-            <h1 className="title-pick">Top Picks</h1>
-            <div className="top-img">
-              {top_pick_data.map((image) => (
-                <div className="top-pick" key={image.id}>
-                    <Link to={`/shop/product/${image.id}`}>
-                    <img  className="img-top-pick" src={image.imageUrl} alt={image.name} />
-                    </Link>
-                    
-                </div>
-              ))}
-                
+  return (
+    <>
+      <Carousel />
+      <BrandCategory />
+      <div className="top-picks-container">
+        <h1 className="title-pick">Top Picks</h1>
+        <div className="top-img">
+          {top_pick_data.map((image) => (
+            <div className="top-pick" key={image.id}>
+              <Link to={`/shop/product/${image.id}`}>
+                <img
+                  className="img-top-pick"
+                  src={image.imageUrl}
+                  alt={image.name}
+                />
+              </Link>
             </div>
-
+          ))}
         </div>
-         <Category/>
-        <div className="JD-select">
-            <h1 className="select-title">JD SELECT</h1>
-            <Link to="/shop/product/11">
-            <img className="select-img" src='/shoes-images/anubhav-arora-g1vk_Bef2Xk-unsplash.jpg' />
-            </Link>
-            
-        </div>
-        <Footer/>   
-
-        </>
-    )
-}
+      </div>
+      <Category />
+      <div className="JD-select">
+        <h1 className="select-title">JD SELECT</h1>
+        <Link to="/shop/product/11">
+          <img
+            className="select-img"
+            src="/shoes-images/anubhav-arora-g1vk_Bef2Xk-unsplash.jpg"
+          />
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;
