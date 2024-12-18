11fc7545f3ab4ff101ebcb3ece221d65b9224643
Sanjay Singh <sanjay007abd@gmail.com>
2024-12-18 13:42:21 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import CategoryPage from './components/pages/categoryPage/categoryPage.component'
-import data from './components/pages/categoryPage/utils.data'
-import NavBar from '../src/components/NavBar/NavBar.components'
-import ProductPage from './components/pages/ProductPage/ProductPage.component'
-import CartPage from './components/pages/cartPage/cartPage.component'
-import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import HomePage from './components/pages/HomePage/HomePage.component'
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.components';
+import data from './components/pages/categoryPage/utils.data';
+
+const HomePage = lazy(() => import('./components/pages/HomePage/HomePage.component'));
+const CategoryPage = lazy(() => import('./components/pages/categoryPage/categoryPage.component'));
+const ProductPage = lazy(() => import('./components/pages/ProductPage/ProductPage.component'));
+const CartPage = lazy(() => import('./components/pages/cartPage/cartPage.component'));
+const SignInAndSignUpPage = lazy(() => import('./components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
 
 function App() {
-  
-
   return (
     <>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path="/shop/:category" element={<CategoryPage  categories = {data}/>}/>
-      <Route path="/shop/product/:id" element={<ProductPage products = {data} />}></Route>
-      <Route path="/shop/cart" element={<CartPage/>}></Route>
-      <Route path="/signin" element={<SignInAndSignUpPage/>}></Route>
-    </Routes>
-      
-
+      <NavBar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop/:category" element={<CategoryPage categories={data} />} />
+          <Route path="/shop/product/:id" element={<ProductPage products={data} />} />
+          <Route path="/shop/cart" element={<CartPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
 
 export default App;
+
